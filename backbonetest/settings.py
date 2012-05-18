6245d9d43f8b1798bc77bb95e2a8ccb936c32068
Meetesh Karia <meetesh.karia@alumni.duke.edu>
2012-05-18 12:05:07 +0200

    use gunicorn

--- backbonetest/settings.py
+++ backbonetest/settings.py
@@ -118,8 +118,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'gunicorn',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
