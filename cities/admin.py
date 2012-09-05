a0ab34acc73b31c542e2567f9b72cebe7dcdb895
Meetesh Karia <meetesh.karia@alumni.duke.edu>
2012-09-04 22:46:01 -0500

    Added admin for cities and states

---
+++ cities/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from cities.models import State, City
+
+admin.site.register(State)
+admin.site.register(City)
--- cities/models.py
+++ cities/models.py
@@ -19,3 +19,6 @@ class City(models.Model):
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = 'cities'
